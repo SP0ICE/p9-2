044b319ed5e1115851af2117ca3fbc542179f229
ilya knyazev <ilak9081@gmail.com>
2023-03-26 13:47:39 +0400

    p9-2

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+size_t ustrlen(const char *);
+int main() {
+    char str[] = "Hello, my name is Ivan";
+    size_t size_str = strlen(str);
+    printf("std_size = %u\n", size_str);
+    size_str = ustrlen(str);
+    printf("usr_size = %u\n", size_str);
+    _getch();
+    return 0;
+}
+size_t ustrlen(const char * p) {
+    size_t size = 0;
+    while (p[size++] != 0) { }
+    return --size;
+}
